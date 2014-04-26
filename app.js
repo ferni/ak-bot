9b64c3cf766615691e066f30f252cf5d947884a4
ferni <fertiberti@gmail.com>
2014-04-26 11:18:15 -0300

    Installed nodewhal

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-var r = require('./reddit');
+/*var r = require('./reddit');
 
 r.login(function() {
     r.comment('t3_23za7o', 'Pero vete al pingo hombre!');
+});*/
+var creds = require('./credentials'),
+    nodewhal = require('nodewhal');
+
+nodewhal('AssKissingBot/0.1 by frrrni').login(creds.user, creds.passwd).then(function(bot) {
+    bot.comment('t3_23za7o', 'Hola, first post as an actual bot!');
 });
