ed2a9cfa7bc7dc91d7798b51606ff6d73e7d269b
ferni <fertiberti@gmail.com>
2014-04-27 01:53:13 -0300

    Replies to friends.

--- app.js
+++ app.js
@@ -2,25 +2,54 @@
 //    r.comment('t3_23za7o'
 var creds = require('./credentials'),
     nodewhal = require('nodewhal'),
-    akb = require('./akb');
+    akb = require('./akb'),
+    _ = require('underscore')._;
 
-var worship = {
-    'LaughableFalafel': {
 
-    }
-};
 
-function addWorship(userName) {
-    worship[userName] = {
-        lastReply: '',
-        commentsRepliedTo: []
-    }
+var lastCommentRepliedTo = 't1_ch33hcb';
+
+function Reply() {
+    this.parentID;
+    this.replyIndex;
 }
 
+function Subscription() {
+    this.username;
+    this.replies;
+    //should only reply to comments after the subscription date
+    this.date;
+}
+
+
+
 nodewhal('AssKissingBot/0.1 by frrrni').login(creds.user, creds.passwd).then(function(bot) {
-    bot.listing('/r/friends/comments', {before: 't1_ch22i5d'}).then(function(comments) {
-        console.log(JSON.stringify(comments));
-    });
+    var options = {};
+    if (lastCommentRepliedTo) {
+        options.before = lastCommentRepliedTo;
+    }
+    setInterval(function() {
+        console.log('Checking new comments');
+        bot.listing('/r/friends/comments', {before: lastCommentRepliedTo}).then(function(comments) {
+            if (_.size(comments) > 0) {
+                console.log('Found ' + _.size(comments) + '!');
+            } else {
+                console.log('Found none.');
+                return;
+            }
+            console.log(JSON.stringify(comments));
+            _.each(comments, function(c, thingID) {
+                console.log('CommentID: ' + thingID + '; Comment text:' + c.body);
+                bot.comment(thingID, akb.random(akb.replies.standard)).then(function(c1) {
+                    console.log('Comment function returns: ' + c1);
+                    console.log('I think I replied: ' + c1.body);
+                    lastCommentRepliedTo = thingID;
+                });
+
+            });
+        });
+    }, 15000);
+
 
     /*setInterval(function() {
         //check friends comments
