985ce0d1bfa54021d838579ea50e097ffb9e9fd5
ferni <fertiberti@gmail.com>
2014-04-27 02:27:18 -0300

    Checks comments only after previous check has been dealt with and in a fixed interval.

--- app.js
+++ app.js
@@ -22,37 +22,55 @@ function Subscription() {
 }
 
 
+var lastCheckTime = new Date(),
+    timeBetweenChecks = 30500;
+
+
 
 nodewhal('AssKissingBot/0.1 by frrrni').login(creds.user, creds.passwd).then(function(bot) {
     var options = {};
     if (lastCommentRepliedTo) {
         options.before = lastCommentRepliedTo;
     }
-    setInterval(function() {
-        console.log('Checking new comments');
-        bot.listing('/r/friends/comments', {before: lastCommentRepliedTo}).then(function(comments) {
-            if (_.size(comments) > 0) {
-                console.log('Found ' + _.size(comments) + '!');
-            } else {
-                console.log('Found none.');
-                return;
-            }
-            console.log(JSON.stringify(comments));
-            _.each(comments, function(c, thingID) {
-                console.log('CommentID: ' + thingID + '; Comment text:' + c.body);
-                bot.comment(thingID, akb.random(akb.replies.standard)).then(function(c1) {
-                    console.log('Comment function returns: ' + c1);
-                    console.log('I think I replied: ' + c1.body);
-                    lastCommentRepliedTo = thingID;
-                });
 
+    function dealWithComments(comments) {
+        var count = _.size(comments),
+            dealtWith = 0;
+        if (count > 0) {
+            console.log('Found ' + count + '!');
+        } else {
+            console.log('Found none.');
+            checkComments();
+        }
+        console.log(JSON.stringify(comments));
+        _.each(comments, function(c, thingID) {
+            console.log('CommentID: ' + thingID + '; Comment text:' + c.body);
+            bot.comment(thingID, akb.random(akb.replies.standard)).then(function(c1) {
+                console.log('Comment function returns: ' + c1);
+                console.log('I think I replied: ' + c1.body);
+                lastCommentRepliedTo = thingID;
+                dealtWith++;
+                if (dealtWith >= count) {
+                    checkComments();
+                }
+            }, function(error) {
+                console.log('There was a problem trying to comment: ' + error);
+                dealtWith++;
+                if (dealtWith >= count) {
+                    checkComments();
+                }
             });
+
         });
-    }, 15000);
+    }
 
+    function checkComments() {
+        setTimeout(function() {
+            lastCheckTime = new Date();
+            console.log('Checking new comments. Time:' + lastCheckTime);
+            bot.listing('/r/friends/comments', {before: lastCommentRepliedTo}).then(dealWithComments);
+        }, timeBetweenChecks - (new Date() - lastCheckTime));
+    }
 
-    /*setInterval(function() {
-        //check friends comments
-
-    }, 5000);*/
+    checkComments();
 });
