ae754007f4e9b2fc82297fcaf3c511326f029947
ferni <fertiberti@gmail.com>
2014-04-29 03:04:48 -0300

    Added some comments.

--- akb.js
+++ akb.js
@@ -7,12 +7,32 @@ akb.replies = {
         'I wish I could upvote you more than once!',
         'I wish I had your eloquence.',
         'You\'re so smart!',
+        'You\'re so funny!',
         'Wow, you blew me away.',
         'I think this is your best comment yet.',
         'You have such wisdom.',
         'How I haven\'t thought of that?!',
-        'If this commnet doesn\'t deserve gold, I don\'t know which does.',
-        'A smashing comment.']
+        'If this doesn\'t deserve gold, I don\'t know which does.',
+        'A smashing comment.',
+        'You are a gentleman and a scholar.',
+        //overly attached
+        '<3',
+        'LOL!',
+        'You\'re an inspiration to me.',
+        'This should be the top comment.',
+        'To the top with you!',
+        'Best comment, hands down.',
+        '/thread'],
+    whenTipping: [
+        'Thanks! We\'re best friends now!',
+        'You made my day, I hope you don\'t mind if I tag along.',
+        'Thank you! Wanna hang out?',
+        'I\'ll follow you till the end!',
+        '(name), you\'re the best.',//generous tip
+        'No, you\'re the best!',//when improving generous tip
+        'Thank you (name). Pretty cool username btw.',
+        'You\'re pretty cool! I bet there\'s lots of interesting stuff in your history.'
+    ]
 };
 
 function randomIntFromInterval(min,max)
@@ -23,6 +43,10 @@ function randomIntFromInterval(min,max)
 akb.random = function(stringArray) {
     var index = randomIntFromInterval(0, stringArray.length - 1);
     return stringArray[index];
+};
+
+akb.getComment = function(parent) {
+
 };
 
 module.exports = akb;
