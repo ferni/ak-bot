3d9f9e140c3902085682dee641d331e3f1ba526f
ferni <fertiberti@gmail.com>
2014-06-16 02:32:48 -0300

    Separated logic to show updated friend list.

--- friends.js
+++ friends.js
@@ -7,14 +7,24 @@ var _ = require('underscore')._;
 module.exports = Friends = (function() {
     var bot, friends = [];
 
+    function printFriends() {
+        var friendList = '';
+        _.each(friends, function(f2) {
+            //f2 structure:
+            /*
+             - name (username)
+             - id (full id)
+             - date (int)
+             */
+            friendList += f2.name + ' ';
+        });
+        console.log('My friends are now: ' + friendList);
+    }
+
     function updateFriends() {
         return bot.get('https://ssl.reddit.com/prefs/friends.json').then(function(f) {
-            var friendList = '';
             friends = f[0].data.children;
-            _.each(friends, function(f2) {
-                friendList += f2.name + ' ';
-            });
-            console.log('My friends are now: ' + friendList);
+            printFriends();
         });
     }
 
